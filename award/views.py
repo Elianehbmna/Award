f026bd0c132da49b2fe45c3f03f44f7d5cc9978b
Elianehbmna <elianehabimana3@gmail.com>
2019-10-25 14:47:55 +0200

    creating the login form

--- award/views.py
+++ award/views.py
@@ -2,10 +2,11 @@
 from __future__ import unicode_literals
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,HttpResponseRedirect
-# from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 # from .forms import ImageForm,UpdateProfile,CommentForm
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'welcome.html')
 
