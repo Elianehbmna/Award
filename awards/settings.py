c1ab736a907eed66f5dd138f1fe18de434a2c813
Elianehbmna <elianehabimana3@gmail.com>
2019-10-25 09:43:25 +0200

    adding bootstrap in the settings.py

--- awards/settings.py
+++ awards/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'award',
+    'bootstrap3',
     'award.apps.AwardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
