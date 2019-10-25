f38d467fba387dd7e87fdaaceb86116644e50a32
Elianehbmna <elianehabimana3@gmail.com>
2019-10-25 15:10:00 +0200

    importing the htmlfiels in the model

--- award/models.py
+++ award/models.py
@@ -5,7 +5,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 import datetime as dt
-# from tinymce.models import HTMLField
+from tinymce.models import HTMLField
 
 class Profile(models.Model):
     bio=models.TextField(max_length=100,blank=True,default="bio please...")
