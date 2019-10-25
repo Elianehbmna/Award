242f6ee772d590efd03fe1cbb88d8994f4bc8742
Elianehbmna <elianehabimana3@gmail.com>
2019-10-25 14:29:58 +0200

    adding the search input

--- award/views.py
+++ award/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
-from django.http  import HttpResponse
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,HttpResponseRedirect
+# from django.contrib.auth.decorators import login_required
+from .models import Profile
+# from .forms import ImageForm,UpdateProfile,CommentForm
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
