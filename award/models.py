6e6189d0b70a7744ae1a58fe8ff23c6b7670d9ae
Elianehbmna <elianehabimana3@gmail.com>
2019-10-26 16:21:47 +0200

    adding the project and rating model

--- award/models.py
+++ award/models.py
@@ -13,6 +13,7 @@ class Profile(models.Model):
     email=models.CharField(blank=True,max_length=100)
     user=models.ForeignKey(User, on_delete=models.CASCADE)
 
+
     def __str__(self):
         return self.user
 
@@ -21,3 +22,38 @@ class Profile(models.Model):
     #     news = cls.objects.filter(user__username__icontains = search_term)
     #     return news
 
+class Project(models.Model):
+    image = models.ImageField(upload_to='landingpage/', blank=True)
+    ProjectName = models.CharField(max_length =30)
+    description= models.TextField(max_length =30)
+    link=models.CharField(max_length=40)
+    screenshot1=models.ImageField(upload_to='screenshots/', blank=True)
+    screenshot2=models.ImageField(upload_to='screenshots/', blank=True)
+    screenshot3=models.ImageField(upload_to='screenshots/', blank=True)
+    design=models.IntegerField(blank=True,default=0)
+    usability=models.IntegerField(blank=True,default=0)
+    content=models.IntegerField(blank=True,default=0)
+    post_date = models.DateTimeField(auto_now_add=True)
+    avatar = models.ImageField(upload_to='avatars/')
+    profile= models.ForeignKey(User)
+    user_profile=models.ForeignKey(Profile)
+    likes=models.ManyToManyField(User,related_name = 'likes',blank=True)
+
+    @classmethod
+    def save_image(self):
+        self.save()
+    @classmethod
+    def delete_image(self):
+        self.delete()
+    @classmethod
+    def update_caption(cls,id,caption):
+        captions=cls.objects.filter(caption_id=id).update(image_caption = caption)
+        return captions
+
+class Rating(models.Model):
+    design = models.IntegerField(blank=True,default=0)
+    usability = models.IntegerField(blank=True,default=0)
+    content = models.IntegerField(blank=True,default=0)
+    overall_score = models.IntegerField(blank=True,default=0)
+    project = models.ForeignKey(Project,on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
